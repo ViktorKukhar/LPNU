f4a18dc5020563a8fbad603e3dbc8d2b6af2c2a0
Viktor Kukhar <109076253+ViktorKukhar@users.noreply.github.com>
2023-06-14 04:42:37 +0300

    Update Prim.rb

--- prim_lab1/Prim.rb
+++ prim_lab1/Prim.rb
@@ -1,32 +1,19 @@
-Rails.application.routes.draw do
-  get "/dashboard" => 'dashboard#index', as: :dashboard
-  get "/admin" => 'admin#admin_panel', as: :admin_panel
-  get "/profile/:id" => 'admin#profile', as: :profile
+class PublicController < ApplicationController
+  def main
+    if account_signed_in?
 
-  get 'dashboard/properties'
-  get 'dashboard/reports'
+    end
 
-  get '/apartments' => 'properties#apartments', as: :apartments
-  get '/houses' => 'properties#houses', as: :houses
+    @properties = Property.latest
 
-  get "/sell" => 'public#sell', as: :sell
-  get "/rent" => 'public#rent', as: :rent
+  end
 
-  get 'sell/apartments' => 'properties#apartments_sell', as: :apartments_sell
-  get 'sell/houses' => 'properties#houses_sell', as: :houses_sell
-  get 'rent/apartments' => 'properties#apartments_rent', as: :apartments_rent
-  get 'rent/houses' => 'properties#houses_rent', as: :houses_rent
+  def sell
+    @properties = Property.latest.where(offer: 'Sell')
+  end
 
-  get 'properties/apartments/1room' => 'properties#one_rooms', as: :one_rooms
-  get 'properties/apartments/2room' => 'properties#two_rooms', as: :two_rooms
-  get 'properties/apartments/3room' => 'properties#three_rooms', as: :three_rooms
-  get 'properties/apartments/4room' => 'properties#four_rooms', as: :four_rooms
+  def rent
+    @properties = Property.latest.where(offer: 'Rent')
+  end
 
-
-
-
-  resources :properties
-  devise_for :accounts
-
-  root to: 'public#main'
 end
