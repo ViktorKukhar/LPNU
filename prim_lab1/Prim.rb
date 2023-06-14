8ca734a65b4d1c196f3a70407e34340287da16f8
Viktor Kukhar <109076253+ViktorKukhar@users.noreply.github.com>
2023-06-14 05:30:24 +0300

    Update Prim.rb

--- prim_lab1/Prim.rb
+++ prim_lab1/Prim.rb
@@ -1,21 +1,77 @@
-<main>
-  <div class="album py-5 bg-light">
-    <div class="container">
-      <section class="latest-properties">
-        <div class="row border-bottom">
-          <div class="col">
-            <h2 class="pb-2">Latest apartments:</h2>
-          </div>
-          <div class="col d-flex justify-content-end ">
-            <%= render "properties/rooms_dropdown" %>
-          </div>
+source "https://rubygems.org"
+git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
-        </div>
+ruby "3.1.2"
 
-        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3 pt-3">
-          <%= render "properties/latest_properties" %>
-        </div>
-      </section>
-    </div>
-  </div>
-</main>
+# Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
+gem "rails", "~> 7.0.3", ">= 7.0.3.1"
+
+gem "devise"
+gem 'sass', '~> 3.4', '>= 3.4.22'
+gem 'sass-rails'
+# gem 'webpack-rails', '~> 0.9.11'
+gem 'foreman', '~> 0.87.2'
+# gem 'webpacker'
+gem 'toastr-rails', '~> 1.0', '>= 1.0.3'
+gem 'jquery-rails'
+gem 'bootstrap', '~> 5.2'
+
+# IMAGE GEM
+gem 'carrierwave', '~> 2.0'
+gem 'mini_magick', '~> 4.5', '>= 4.5.1'
+gem 'image_processing', '~> 1.12', '>= 1.12.2'
+
+
+# The original asset pipeline for Rails [https://github.com/rails/sprockets-rails]
+gem "sprockets-rails"
+
+# Use postgresql as the database for Active Record
+gem "pg", "~> 1.1"
+
+# Use the Puma web server [https://github.com/puma/puma]
+gem "puma", "~> 5.0"
+
+# Use JavaScript with ESM import maps [https://github.com/rails/importmap-rails]
+gem "importmap-rails"
+
+# Hotwire's SPA-like page accelerator [https://turbo.hotwired.dev]
+gem "turbo-rails"
+
+# Hotwire's modest JavaScript framework [https://stimulus.hotwired.dev]
+gem "stimulus-rails"
+
+# Build JSON APIs with ease [https://github.com/rails/jbuilder]
+gem "jbuilder"
+
+# Use Kredis to get higher-level data types in Redis [https://github.com/rails/kredis]
+# gem "kredis"
+
+# Use Active Model has_secure_password [https://guides.rubyonrails.org/active_model_basics.html#securepassword]
+# gem "bcrypt", "~> 3.1.7"
+
+# Windows does not include zoneinfo files, so bundle the tzinfo-data gem
+gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
+
+# Reduces boot times through caching; required in config/boot.rb
+gem "bootsnap", require: false
+
+# Use Sass to process CSS
+gem "sassc-rails"
+
+group :development, :test do
+  # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
+  gem "debug", platforms: %i[ mri mingw x64_mingw ]
+end
+
+group :development do
+  # Use console on exceptions pages [https://github.com/rails/web-console]
+  gem "web-console"
+
+  # Add speed badges [https://github.com/MiniProfiler/rack-mini-profiler]
+  # gem "rack-mini-profiler"
+
+  # Speed up commands on slow machines / big apps [https://github.com/rails/spring]
+  # gem "spring"
+
+  gem "figaro"
+end
