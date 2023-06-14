7a7fbc090ee0e6571d7616c5b6822d4021da23bf
Viktor Kukhar <109076253+ViktorKukhar@users.noreply.github.com>
2023-06-14 04:34:42 +0300

    Update Prim.rb

--- prim_lab1/Prim.rb
+++ prim_lab1/Prim.rb
@@ -1,22 +1,32 @@
-<h2 class="mt-5">Sellers:</h2>
-<table class= "table table-striped">
+Rails.application.routes.draw do
+  get "/dashboard" => 'dashboard#index', as: :dashboard
+  get "/admin" => 'admin#admin_panel', as: :admin_panel
+  get "/profile/:id" => 'admin#profile', as: :profile
 
-  <thead>
-    <tr>
-      <th>Name</th>
-      <th>Email</th>
-      <th>Created at:</th>
-      <th>Properties:</th>
-    </tr>
-  </thead>
-  <tbody>
-    <% @accounts.each do |account| %>
-    <tr>
-    <td><%= account.full_name %></td>
-    <td><%= account.email %></td>
-    <td><%= account.created_at.strftime("%d/%m/%Y") %></td>
-    <td><%= link_to 'View', profile_path(account), class: "btn btn-outline-primary btn-sm mx-2" %></td>
-    </tr>
-    <% end %>
-  </tbody>
-</table>
+  get 'dashboard/properties'
+  get 'dashboard/reports'
+
+  get '/apartments' => 'properties#apartments', as: :apartments
+  get '/houses' => 'properties#houses', as: :houses
+
+  get "/sell" => 'public#sell', as: :sell
+  get "/rent" => 'public#rent', as: :rent
+
+  get 'sell/apartments' => 'properties#apartments_sell', as: :apartments_sell
+  get 'sell/houses' => 'properties#houses_sell', as: :houses_sell
+  get 'rent/apartments' => 'properties#apartments_rent', as: :apartments_rent
+  get 'rent/houses' => 'properties#houses_rent', as: :houses_rent
+
+  get 'properties/apartments/1room' => 'properties#one_rooms', as: :one_rooms
+  get 'properties/apartments/2room' => 'properties#two_rooms', as: :two_rooms
+  get 'properties/apartments/3room' => 'properties#three_rooms', as: :three_rooms
+  get 'properties/apartments/4room' => 'properties#four_rooms', as: :four_rooms
+
+
+
+
+  resources :properties
+  devise_for :accounts
+
+  root to: 'public#main'
+end
